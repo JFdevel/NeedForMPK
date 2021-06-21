81ead38183d792f397976ef1a372953e09369f51
JFdevel <39263181+JFdevel@users.noreply.github.com>
2021-06-21 19:34:21 +0500

    add all

---
+++ Assets/ECCAssets/Scripts/changelvl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class changelvl : MonoBehaviour
+{
+    public GameObject temp;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+	private void Awake()
+	{
+        temp.gameObject.SetActive(false);
+	}
+}
